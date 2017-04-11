7a982b29ad4ab59cbbebdac816fc05bafffb0a24
Bogdan Demchenko <bogdandemchenko@gmail.com>
2017-04-11 22:22:07 +0300

    add blogContr@getIndex method

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -7,10 +7,19 @@ use App\Post;
 
 class BlogController extends Controller
 {
+	public function getIndex()
+	{
+		$posts = Post::paginate(10);
+
+		return view(blog.index)->withPosts($posts);
+		// return "blog.index";
+	}
+
 	public function getSingle($slug)
 	{
 		$post = Post::where('slug', '=', $slug)->first();
 
 		return view('blog.single')->withPost($post);
 	}
+
 }
