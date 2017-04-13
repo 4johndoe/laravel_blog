371f56e2f194b837a7d174f046b8ee6b84a49c17
bogdan demchenko <bogdandemchenko@gmail.com>
2017-04-13 22:20:33 +0300

    dbdump

--- lar_blog.sql
+++ lar_blog.sql
@@ -119,7 +119,7 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,6 +128,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'bogdan demchenko','bogdandemchenko@gmail.com','$2y$10$urwVlySWewQGzdrh.aYeHuc8RoTwqgehptWsja2g2x7pQC/c4HTku',NULL,'2017-04-13 07:58:42','2017-04-13 07:58:42');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -140,4 +141,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-04-12 17:57:33
+-- Dump completed on 2017-04-13 22:20:05
