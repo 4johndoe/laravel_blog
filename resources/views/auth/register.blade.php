39d740ff822ac38a91bc7b7491f4f7342b7d69cc
bogdan demchenko <bogdandemchenko@gmail.com>
2017-04-13 17:47:40 +0300

    login/logout check +

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
 		<div class="col-md-6 col-md-offset-3">
 			{{ Form::open() }}
 				{{ Form::label('name', 'Name:') }}
-				{{ Form::email('email', null, ['class' => 'form-control']) }}
+				{{ Form::text('name', null, ['class' => 'form-control']) }}
 				
 				{{ Form::label('email', 'Email:') }}
 				{{ Form::email('email', null, ['class' => 'form-control']) }}
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,12 @@
 ###################
 // auth routen
 Route::get('auth/login', 'Auth\LoginController@showLoginForm');
-// Route::post('auth/login', 'Auth\LoginController@postLogin');
-// Route::get('auth/logout', 'Auth\LoginController@getLogout');
+Route::post('auth/login', 'Auth\LoginController@login');
+Route::get('auth/logout', 'Auth\LoginController@logout');
 
 // register routes
 Route::get('auth/register', 'Auth\RegisterController@showRegistrationForm');
-Route::post('auth/register', 'Auth\RegisterController@postRegister');
+Route::post('auth/register', 'Auth\RegisterController@register');
 ###################
 Route::get('/blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])->where('slug', '[\w\d\-\_]+');
 Route::get('/blog', ['uses' => 'BlogController@getIndex', 'as' => 'blog.index']);
